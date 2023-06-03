e7f4facbf2cc3890b4c5048f433d5b1a68dfb10f
Nathan4Andrade <nathanandrade20@gmail.com>
2023-06-03 17:57:19 -0300

    refatorando

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import HabitosPage from "./pages/HabitosPage/HabitosPage";
 import HojePage from "./pages/HojePage/HojePage";
 import HistoricoPage from "./pages/HistoricoPage/HistoricoPage";
 import Navbar from "./components/Navbar";
-import { AuthProvider } from "./contexts/AuthContext";
 import ScrollToTop from "./components/ScrollToTop";
 
 import { ContextProvider } from "./contexts/Context";
